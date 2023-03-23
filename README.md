9c8b199e6c54a21a624acf72697e4ecf38ad10b5
ProudWadhwa <proud@gftpl.in>
2023-03-23 16:23:44 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# A script to deploy django projects on ubuntu & debian.
+  1. To deploy a django project on non-www or subdomain, you can use the specific file deploy.py helps in this function. This file will deploy django project on provided domain/sub-domain.
+  2. To Deploy a django project on www.domain.com you can use a specific file deploy-www.py.
+
+## How to setup project?
+  1. Cloning the project and setting up the db.
+  2. Virtualenv - venv, This should be your virtualenvironment name because this is set static in py file.
+  3. Install Requirements along with gunicorn to deploy this project.
+
+## Basic Operations to use both files.
+  1. `Enter the location of the project directory:` - Refers to the project location of project you want to deploy. E.g. `/home/user/project`.
+  2. `Enter the Project name:` - Refers to the project name which will be the sock, service and nginx file name. E.g. `project`.
+  3. `Enter the name of the inner Django project directory:` - Enter the main project name of the django project which is located inside the main directory or with the manage.py. E.g. `inner_project`.
+  4. `Enter the domain name:` - Asks the domain/subdomain name to deploy the django project on. E.g. `example.com`.
+  5. `Enter the username:` - Asks the username of the server to add it into gunicorn config. E.g. `user`.
+  
+
+**That's It all over**, Now the file will execute necessary commands and make your project live on provided domain.
+
+## Troubleshooting
+  1. 502 Bad Gateway: Nginx gives error in this case when the gunicorn file is not working properly. Use `sudo systemctl status project_name.service` this command will show you the error. Majorily the error could be of not installing gunicorn in venv or not finding any other module. This can be also occur due to wrong path, project name or inner project name.
+  2. 404 Not Found: Maybe you've entered wrong path, project name or inner project name. To solve this delete gunicorn and nginx files and re-run deploy py file.
+    a) Gunicorn Service file path - `/etc/systemd/system/project_name.service`
+    b) Nginx Config. File Path - `/etc/nginx/sites-enabled/project_name`
+    
+  **Note: These files can be vary according to system/os. This is of debian and ubuntu**
+ 
---
+++ deploy.py
@@ -0,0 +1,80 @@
+import os
+
+# Get user input
+project_location = input("Enter the location of the project directory: ")
+project_name = input("Enter the Project name: ")
+inner_project_name = input("Enter the name of the inner Django project directory: ")
+domain_name = input("Enter the domain name: ")
+username = input("Enter the username: ")
+
+# Create Gunicorn service file
+gunicorn_service = f"""[Unit]
+Description=gunicorn daemon for {project_name}
+After=network.target
+
+[Service]
+User={username}
+Group=www-data
+WorkingDirectory={project_location}
+ExecStart={project_location}/venv/bin/gunicorn --access-logfile - --workers 3 --bind unix:{project_location}/{project_name}.sock {inner_project_name}.wsgi:application
+
+[Install]
+WantedBy=multi-user.target
+"""
+
+# Create and save Gunicorn service file
+with open(f"{project_name}.service", "w") as f:
+    f.write(gunicorn_service)
+
+# Create Nginx server block
+nginx_block = f"""
+server {{
+    server_name {domain_name};
+
+    location = /favicon.ico {{ access_log off; log_not_found off; }}
+    location /static/ {{
+        root {project_location};
+    }}
+    location /media/ {{
+        root {project_location};
+    }}
+    access_log /var/log/nginx/{project_name}-access.log;
+    error_log /var/log/nginx/{project_name}-error.log;
+    location / {{
+        include proxy_params;
+        proxy_pass http://unix:{project_location}/{project_name}.sock;
+    }}
+
+    listen 80; # managed by Certbot
+
+}}
+
+server {{
+    if ($host = {domain_name}) {{
+        return 301 https://{domain_name}/;
+    }} # managed by Certbot
+    
+    server_name {domain_name};
+
+    listen 80;
+    return 404; # managed by Certbot
+
+
+}}
+
+"""
+
+# Create and save Nginx server block
+with open(f"{project_name}", "w") as f:
+    f.write(nginx_block)
+
+# Restart Nginx and Gunicorn
+os.system(f"sudo mv {project_name}.service /etc/systemd/system/")
+os.system(f"sudo mv {project_name} /etc/nginx/sites-enabled/")
+
+os.system("sudo systemctl daemon-reload")
+os.system(f"sudo systemctl start {project_name}.service")
+os.system(f"sudo systemctl enable {project_name}.service")
+os.system("sudo systemctl restart nginx")
+
+print(f"\033[1;32;40m \n This Program had deployed {project_name} on {domain_name} \n If there is any error make sure to check the readme.MD Troubleshooting part.")
